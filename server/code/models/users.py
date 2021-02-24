8c3c52405eb3a9d2c0b910e118223a204aa2696c
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-24 09:05:02 -0500

    Change UserModel id field to StringField (#6)

--- server/code/models/users.py
+++ server/code/models/users.py
@@ -2,6 +2,7 @@
 import mongoengine as me
 
 # project imports
+from models.default_values import uuid_value
 from models.shared.base_mixin import BaseMixin
 from models.user_types import UserTypeModel
 
@@ -10,7 +11,7 @@ class UserModel(BaseMixin, me.Document):
     Users
     '''
 
-    id = me.SequenceField(primary_key=True)
+    id = me.StringField(primary_key=True, default=uuid_value)
     first_name = me.StringField(required=True)
     last_name = me.StringField(required=True)
     email_address = me.EmailField(required=True, unique=True)
