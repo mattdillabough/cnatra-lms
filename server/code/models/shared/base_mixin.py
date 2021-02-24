88bc4a9f9f0d8bb10ce80b29f01a4a352ef82c48
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-24 08:47:40 -0500

    Add created_at and updated_at columns to BaseMixin (#6)

--- server/code/models/shared/base_mixin.py
+++ server/code/models/shared/base_mixin.py
@@ -2,14 +2,16 @@
 import mongoengine as me
 
 # project imports
-from models.default_values import default_uuid_value
+from models.default_values import uuid_value, current_timestamp
 
 class BaseMixin(me.Document):
     '''
     Base model from which all other tables inherit
     '''
 
-    uuid = me.StringField(default=default_uuid_value)
+    guid = me.StringField(default=default_uuid_value)
+    created_at = me.DateTimeField(default=current_timestamp)
+    updated_at = me.DateTimeField(default=current_timestamp)
 
 
     # allows for 'inheritance' while allowing other models to be created as separate collections
