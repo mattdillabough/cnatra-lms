cbeb8fe15a95fd851c7321cbfdaa2f8faf4b0caf
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-25 06:13:14 -0500

    Add StageModel (#6)

---
+++ server/code/models/stages.py
@@ -0,0 +1,21 @@
+# python imports
+import mongoengine as me
+
+# project imports
+from models.default_values import uuid_value
+from models.stage_types import StageTypeModel
+from models.shared.base_mixin import BaseMixin
+from models.shared.start_date_end_date_mixin import StartDateEndDateMixin
+
+class StageModel(BaseMixin, StartDateEndDateMixin, me.Document):
+    '''
+    Stages
+    '''
+
+    id = me.StringField(primary_key=True, default=uuid_value)
+    stage = me.StringField(null=False)
+    stage_type = me.ReferenceField(StageTypeModel)
+
+
+    meta = {'collection': 'stages'}
+
