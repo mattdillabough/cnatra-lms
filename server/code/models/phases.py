e1efbad3fe3d08f3a9b0e9c749db0bc962e2f58e
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-25 06:20:28 -0500

    Add pipeline column (ReferenceField) to phases (#6)

--- server/code/models/phases.py
+++ server/code/models/phases.py
@@ -4,6 +4,7 @@ import mongoengine as me
 # project imports
 from models.default_values import uuid_value
 from models.phase_types import PhaseTypeModel
+from models.pipelines import PipelineModel
 from models.shared.base_mixin import BaseMixin
 from models.shared.start_date_end_date_mixin import StartDateEndDateMixin
 
@@ -15,6 +16,7 @@ class PhaseModel(BaseMixin, StartDateEndDateMixin, me.Document):
     id = me.StringField(primary_key=True, default=uuid_value)
     phase = me.StringField(null=False)
     phase_type = me.ReferenceField(PhaseTypeModel)
+    pipeline = me.ReferenceField(PipelineModel)
 
 
     meta = {'collection': 'phases'}
