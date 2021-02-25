a92ff7d8a3991ec25b3e477cbaff6bb1011e548b
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-25 06:25:54 -0500

    Add phase (ReferenceField) to StageModel (#6)

--- server/code/models/stages.py
+++ server/code/models/stages.py
@@ -3,6 +3,7 @@ import mongoengine as me
 
 # project imports
 from models.default_values import uuid_value
+from models.phases import PhaseModel
 from models.stage_types import StageTypeModel
 from models.shared.base_mixin import BaseMixin
 from models.shared.start_date_end_date_mixin import StartDateEndDateMixin
@@ -15,6 +16,7 @@ class StageModel(BaseMixin, StartDateEndDateMixin, me.Document):
     id = me.StringField(primary_key=True, default=uuid_value)
     stage = me.StringField(null=False)
     stage_type = me.ReferenceField(StageTypeModel)
+    phase = me.ReferenceField(PhaseModel)
 
 
     meta = {'collection': 'stages'}
