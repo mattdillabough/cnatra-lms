e08d51b17dedb1a132a8510fdb9a13cb36669442
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-25 05:51:46 -0500

    Add PipelineModel (#6)

---
+++ server/code/models/pipelines.py
@@ -0,0 +1,20 @@
+# python imports
+import mongoengine as me
+
+# project imports
+from models.default_values import uuid_value
+from models.pipeline_types import PipelineTypeModel
+from models.shared.base_mixin import BaseMixin
+from models.shared.start_date_end_date_mixin import StartDateEndDateMixin
+
+class PipelineModel(BaseMixin, StartDateEndDateMixin, me.Document):
+    '''
+    Pipelines
+    '''
+
+    id = me.StringField(primary_key=True, default=uuid_value)
+    pipeline_type = me.ReferenceField(PipelineTypeModel)
+
+
+    meta = {'collection': 'pipelines'}
+
