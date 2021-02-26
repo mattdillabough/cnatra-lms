08ccd749523f7a7fef073f0c67afe68bb5d8cbbb
Victoria <vicglee1234007@gmail.com>
2021-02-26 02:16:07 -0600

    Create routes to different components (#8)

---
+++ frontend/src/Routes.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {withRouter, Route, Switch} from "react-router-dom";
+
+import Home from "./Components/Home";
+import Academics from "./Components/Academics";
+import Grades from "./Components/Grades";
+import Settings from "./Components/Settings";
+import Login from "./Components/Login";
+
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/Academics" component={Academics}/>
+      <Route path="/Grades" component={Grades}/>
+      <Route path="/Settings" component={Settings}/>
+      <Route path="/Login" component={Login}/>
+    </Switch>
+  )
+}
+
+//Todo: If the user is not logged in yet, they should be redirected to the Login page
+
+export default withRouter(Routes)
