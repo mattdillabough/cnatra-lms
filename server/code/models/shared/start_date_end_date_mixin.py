4c9cb41f7a9fea533b9622ed620f30a96b206423
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-26 18:50:01 -0500

    Add required attribute to StartDateEndDateMixin (#6)

--- server/code/models/shared/start_date_end_date_mixin.py
+++ server/code/models/shared/start_date_end_date_mixin.py
@@ -8,8 +8,8 @@ class StartDateEndDateMixin(me.Document):
     Adds date columns to table to indicate start and end
     '''
 
-    start_date = me.DateTimeField()
-    end_date = me.DateTimeField()
+    start_date = me.DateTimeField(required=True)
+    end_date = me.DateTimeField(required=True)
 
 
     # allows for 'inheritance' while allowing other models to be created as separate collections
