89ff708557b60c16df86709f91a1f64d62ff4f35
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-26 18:23:59 -0500

    Remove PipelineTypeModel reference from PipelineModel (#6)

--- server/code/models/pipelines.py
+++ server/code/models/pipelines.py
@@ -3,7 +3,6 @@ import mongoengine as me
 
 # project imports
 from models.default_values import uuid_value
-from models.pipeline_types import PipelineTypeModel
 from models.shared.base_mixin import BaseMixin
 from models.shared.start_date_end_date_mixin import StartDateEndDateMixin
 
@@ -14,7 +13,6 @@ class PipelineModel(BaseMixin, StartDateEndDateMixin, me.Document):
 
     id = me.StringField(primary_key=True, default=uuid_value)
     pipeline = me.StringField(null=False)
-    pipeline_type = me.ReferenceField(PipelineTypeModel)
 
 
     meta = {'collection': 'pipelines'}
