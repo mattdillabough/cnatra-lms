33b5c9d88e8e462dd1caff97ebb984e12967ee7e
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-26 18:50:52 -0500

    Remove PhaseTypeModel reference from PhaseModel (#6)

--- server/code/models/phases.py
+++ server/code/models/phases.py
@@ -3,7 +3,6 @@ import mongoengine as me
 
 # project imports
 from models.default_values import uuid_value
-from models.phase_types import PhaseTypeModel
 from models.pipelines import PipelineModel
 from models.shared.base_mixin import BaseMixin
 from models.shared.start_date_end_date_mixin import StartDateEndDateMixin
@@ -15,7 +14,6 @@ class PhaseModel(BaseMixin, StartDateEndDateMixin, me.Document):
 
     id = me.StringField(primary_key=True, default=uuid_value)
     phase = me.StringField(null=False)
-    phase_type = me.ReferenceField(PhaseTypeModel)
     pipeline = me.ReferenceField(PipelineModel)
 
 
