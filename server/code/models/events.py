7fff0d94f336be69558bd4fa48a8753ca860f142
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-02-28 16:25:30 -0500

    Add EventModel (#6)

---
+++ server/code/models/events.py
@@ -0,0 +1,20 @@
+# python imports
+import mongoengine as me
+
+# project imports
+from models.blocks import BlockModel
+from models.medias import MediaModel
+from models.shared.base_mixin import BaseMixin
+
+class EventModel(BaseMixin, me.Document):
+    '''
+    Events
+    '''
+
+    event_number = me.StringField(primary_key=True)
+    media = me.ReferenceField(MediaModel)
+    block = me.ReferenceField(BlockModel)
+    title = me.StringField()
+    hours = me.DecimalField()
+
+    meta = {'collection': 'events'}
