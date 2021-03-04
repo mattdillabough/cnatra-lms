eabbda8d1e933c6fab1dc45e0483eea0dcbf85d9
Victoria <vicglee1234007@gmail.com>
2021-03-04 03:03:27 -0600

    Maneuver components are expandable & collapsible (#8)

--- frontend/src/Components/Grading/Maneuver.js
+++ frontend/src/Components/Grading/Maneuver.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 import {RiArrowDownSFill} from "react-icons/ri";
 
 
 function Maneuver({maneuver}){
   // console.log("props?", maneuver)
+  const [dropdown, setDropDown] = useState(false);
+  const toggleDropDown = () => {setDropDown(!dropdown)}
+
   return(
     <div className="maneuver container-fluid">
         <div className="maneuver-header">
           <div>{maneuver.description|| "Maneuver Name"}</div>
           <div>{maneuver.grade||"Grade"}</div>
           <div>{maneuver.MIF||"MIF"}</div>
-          <button type="button"><RiArrowDownSFill/></button>
+          <button type="button" className="toggle-maneuver btn" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" onClick={toggleDropDown}><RiArrowDownSFill/></button>
         </div>
-        <div className="maneuver-body">
+        <div className={dropdown ? "maneuver-body collapse show" : "maneuver-body collapse" } id="collapseExample" aria-expanded="false" aria-controls="collapseExample">
           <div>{maneuver.comments||"No comments"}</div>
         </div>
       </div>
