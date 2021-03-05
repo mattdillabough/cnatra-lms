d7b7e5575decece47e1d12cdf129507f37f0acf2
Victoria <vicglee1234007@gmail.com>
2021-03-04 19:48:33 -0600

    Begin creating event gradesheet details structure (#8)

--- frontend/src/Components/Grading/Gradesheet.js
+++ frontend/src/Components/Grading/Gradesheet.js
@@ -4,7 +4,7 @@ import React from "react";
 
 import Maneuver from "./Maneuver";
 
-function Gradesheet(){
+function Gradesheet() {
   const mockGradesheetData = {
     eventId: "H3l1c0p73r",
     title: "Helicopter Flight",
@@ -65,55 +65,77 @@ function Gradesheet(){
       },
       approaches: {
         typeNo: "",
-      }
+      },
     },
     comments: "Student was amazing!",
     maneuvers: [
-     { id: 1,
-      description: "General Knowledge / Procedures",
-      MIF: "4+",
-      grade: "G", //corresponds to 4,
-      comments: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+      {
+        id: 1,
+        description: "General Knowledge / Procedures",
+        MIF: "4+",
+        grade: "G", //corresponds to 4,
+        comments: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       },
-     { id: 2,
-      description: "Emer Procedures / Sys Failure",
-      MIF: "4+",
-      grade: "U", //corresponds to 2
-      comments: "",
+      {
+        id: 2,
+        description: "Emer Procedures / Sys Failure",
+        MIF: "4+",
+        grade: "U", //corresponds to 2
+        comments: "",
       },
-     { id: 3,
-      description: "No Hover Takeoff",
-      MIF: "4+",
-      grade: "F", //corresponds to 3
-      comments: "Praesent tempus ligula suscipit mauris pretium, id sodales ligula placerat. Nulla malesuada laoreet bibendum. Nam a dapibus lacus, sed pellentesque diam. Proin ligula lorem, hendrerit in facilisis id, sagittis non magna. Aliquam pellentesque ut tellus non ornare. Integer suscipit dui ut quam aliquet ultricies. Pellentesque auctor odio ut tellus maximus suscipit. Donec finibus risus euismod tellus ullamcorper aliquam. Integer dolor tellus, rutrum et risus id, lobortis consequat sem."
+      {
+        id: 3,
+        description: "No Hover Takeoff",
+        MIF: "4+",
+        grade: "F", //corresponds to 3
+        comments:
+          "Praesent tempus ligula suscipit mauris pretium, id sodales ligula placerat. Nulla malesuada laoreet bibendum. Nam a dapibus lacus, sed pellentesque diam. Proin ligula lorem, hendrerit in facilisis id, sagittis non magna. Aliquam pellentesque ut tellus non ornare. Integer suscipit dui ut quam aliquet ultricies. Pellentesque auctor odio ut tellus maximus suscipit. Donec finibus risus euismod tellus ullamcorper aliquam. Integer dolor tellus, rutrum et risus id, lobortis consequat sem.",
       },
-     { id: 4,
-      description: "Low-level Navigation",
-      MIF: "4+",
-      grade: "E", //corresponds to 5
-      comments: "Nam quis sollicitudin lectus. Sed mi elit, sollicitudin at mi eu, dapibus pharetra orci. Etiam eu neque vitae mauris mollis interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus."
+      {
+        id: 4,
+        description: "Low-level Navigation",
+        MIF: "4+",
+        grade: "E", //corresponds to 5
+        comments:
+          "Nam quis sollicitudin lectus. Sed mi elit, sollicitudin at mi eu, dapibus pharetra orci. Etiam eu neque vitae mauris mollis interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
       },
-     { id: 5,
-      description: "Special Syllabus Requirements",
-      MIF: "1",
-      grade: "N", //corresponds to 1 (not graded)
-      comments: "Nulla at congue orci. Nulla ut leo ipsum. Sed fringilla libero a magna aliquet sodales. Nam feugiat quam vitae congue hendrerit. Suspendisse ac eros euismod nisi lacinia ultricies. Ut vel tempus justo, eu consectetur sapien. Suspendisse at gravida tellus. Vivamus placerat maximus ligula nec facilisis. Etiam suscipit at risus in luctus. In hac habitasse platea dictumst. Etiam sit amet leo et sapien hendrerit fringilla. Sed facilisis condimentum odio in luctus. Suspendisse tempus malesuada felis ut volutpat. Etiam eget ex convallis, feugiat neque eget, scelerisque orci. Nunc at vestibulum tellus, quis mattis ex. Pellentesque suscipit metus dui, a luctus velit tincidunt sit amet."
+      {
+        id: 5,
+        description: "Special Syllabus Requirements",
+        MIF: "1",
+        grade: "N", //corresponds to 1 (not graded)
+        comments:
+          "Nulla at congue orci. Nulla ut leo ipsum. Sed fringilla libero a magna aliquet sodales. Nam feugiat quam vitae congue hendrerit. Suspendisse ac eros euismod nisi lacinia ultricies. Ut vel tempus justo, eu consectetur sapien. Suspendisse at gravida tellus. Vivamus placerat maximus ligula nec facilisis. Etiam suscipit at risus in luctus. In hac habitasse platea dictumst. Etiam sit amet leo et sapien hendrerit fringilla. Sed facilisis condimentum odio in luctus. Suspendisse tempus malesuada felis ut volutpat. Etiam eget ex convallis, feugiat neque eget, scelerisque orci. Nunc at vestibulum tellus, quis mattis ex. Pellentesque suscipit metus dui, a luctus velit tincidunt sit amet.",
       },
-    ]
-  }
+    ],
+  };
 
-  return(
+  return (
     <div className="Gradesheet container">
-      <h4 className="event-identifier">{`[${mockGradesheetData.eventId}] - ${mockGradesheetData.title}`||"[Event ID] - [Event Name]"}</h4>
-     <div>Event Details</div>
-     <div>Will also likely be a drop-down...Suspendisse at gravida tellus. Vivamus placerat maximus ligula nec facilisis. Etiam suscipit at risus in luctus. In hac habitasse platea dictumst. Etiam sit amet leo et sapien hendrerit fringilla. Sed facilisis condimentum odio in luctus. Suspendisse tempus malesuada felis ut volutpat. Etiam eget ex convallis, feugiat neque eget, scelerisque orci. Nunc at vestibulum tellus, quis mattis ex. Pellentesque suscipit metus dui, a luctus velit tincidunt sit amet.</div>
-      {mockGradesheetData.maneuvers.map((maneuver)=>{
-        return(
-          <Maneuver key={maneuver.id} maneuver={maneuver}/>
-        )
+      <h4 className="event-identifier">
+        {`[${mockGradesheetData.eventId}] - ${mockGradesheetData.title}` ||
+          "[Event ID] - [Event Name]"}
+      </h4>
+      <span>{mockGradesheetData.date}</span>
+      <span>{mockGradesheetData.status}</span>
+      <div className="gradesheet-details container-fluid">
+        <div className="details-header">Event Details</div>
+        <div className="details-body">
+          Will also likely be a drop-down...Suspendisse at gravida tellus.
+          Vivamus placerat maximus ligula nec facilisis. Etiam suscipit at risus
+          in luctus. In hac habitasse platea dictumst. Etiam sit amet leo et
+          sapien hendrerit fringilla. Sed facilisis condimentum odio in luctus.
+          Suspendisse tempus malesuada felis ut volutpat. Etiam eget ex
+          convallis, feugiat neque eget, scelerisque orci. Nunc at vestibulum
+          tellus, quis mattis ex. Pellentesque suscipit metus dui, a luctus
+          velit tincidunt sit amet.
+        </div>
+      </div>
+      {mockGradesheetData.maneuvers.map((maneuver) => {
+        return <Maneuver key={maneuver.id} maneuver={maneuver} />;
       })}
     </div>
-  )
+  );
 }
 
 export default Gradesheet;
