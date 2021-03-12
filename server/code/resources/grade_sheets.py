8e59ab1f1ec894517df0c20a2cc8d5f253f20043
Aneesh Kodali <aneesh.kodali@gmail.com>
2021-03-12 05:36:26 -0500

    Retrieve grade_sheet_maneuvers in grade_sheet resource (#9)

--- server/code/resources/grade_sheets.py
+++ server/code/resources/grade_sheets.py
@@ -2,6 +2,7 @@
 from flask_restful import Resource
 
 # project imports
+from models.grade_sheet_maneuvers import GradeSheetManeuverModel
 from models.grade_sheets import GradeSheetModel
 
 class GradeSheet(Resource):
@@ -11,6 +12,9 @@ class GradeSheet(Resource):
         # query for grade_sheet
         grade_sheet_model = GradeSheetModel.objects(grade_sheet_id=grade_sheet_id).first()
         grade_sheet = grade_sheet_model.as_dict()
+
+        # get grade_sheet grade_sheet_maneuvers
+        grade_sheet['grade_sheet_maneuvers'] = [grade_sheet_maneuver.as_dict() for grade_sheet_maneuver in GradeSheetManeuverModel.objects(grade_sheet=grade_sheet_model)]
 
         # return grade_sheet or 404 (not found)
         if grade_sheet:
