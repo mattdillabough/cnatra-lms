e89b14d26df873ab65b79aa83b3e6f05db887ceb
Victoria <vicglee1234007@gmail.com>
2021-04-01 17:34:17 -0500

    Set up redux store to include student data (#8)

--- frontend/src/Store/index.js
+++ frontend/src/Store/index.js
@@ -4,14 +4,16 @@ import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 //Reducers
-import userReducer from "./users";
+// import userReducer from "./users";
 import navigation from "./navigation";
+import students from "./students";
 import grades from "./grades";
 import formControls from "./formControl";
 
 //This determines what the props are called when state is referenced within a connected component
 const reducer = combineReducers({
-  userDetails: userReducer,
+  // userDetails: userReducer,
+  students,
   navigation,
   grades,
   formControls,
