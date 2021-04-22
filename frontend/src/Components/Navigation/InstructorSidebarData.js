99dfe654de6afea3c2106dd5aeed054d99e9c929
Victoria <vicglee1234007@gmail.com>
2021-04-22 16:07:35 -0500

    Add comments (#8)

--- frontend/src/Components/Navigation/InstructorSidebarData.js
+++ frontend/src/Components/Navigation/InstructorSidebarData.js
@@ -1,52 +1,56 @@
-import React from "react"
-import {FaHome, FaGraduationCap} from "react-icons/fa"
-import {RiBookMarkLine, RiArrowDownSFill, RiArrowUpSFill } from "react-icons/ri"
-import {IoSettingsOutline} from "react-icons/io5"
-import {IoIosPaper} from "react-icons/io";
+import React from "react";
+import { FaHome, FaGraduationCap } from "react-icons/fa";
+import {
+  RiBookMarkLine,
+  RiArrowDownSFill,
+  RiArrowUpSFill,
+} from "react-icons/ri";
+import { IoSettingsOutline } from "react-icons/io5";
+import { IoIosPaper } from "react-icons/io";
 
-
+//Change file to include more/different Navbar links
 const SidebarData = [
   {
     title: "Home",
     path: "/",
-    icon: <FaHome title="Home"/>,
+    icon: <FaHome title="Home" />,
     cName: "nav-text",
   },
   {
     title: "Academics",
     path: "/Academics",
-    icon: <FaGraduationCap title="Academics"/>,
+    icon: <FaGraduationCap title="Academics" />,
     cName: "nav-text",
 
-    iconClosed: <RiArrowDownSFill/>,
-    iconOpened: <RiArrowUpSFill/>,
+    iconClosed: <RiArrowDownSFill />,
+    iconOpened: <RiArrowUpSFill />,
 
     subNav: [
       {
         title: "Phase 1",
         path: "/Academics/Phase-1",
-        icon: <IoIosPaper />
+        icon: <IoIosPaper />,
       },
       {
         title: "Phase 2",
         path: "/Academics/Phase-2",
-        icon: <IoIosPaper />
+        icon: <IoIosPaper />,
       },
-    ]
+    ],
   },
 
   {
     title: "Gradebooks",
     path: "/Grades",
-    icon: <RiBookMarkLine title="Gradebooks"/>,
+    icon: <RiBookMarkLine title="Gradebooks" />,
     cName: "nav-text",
   },
   {
     title: "Settings",
     path: "/Settings",
-    icon: <IoSettingsOutline title="Settings"/>,
+    icon: <IoSettingsOutline title="Settings" />,
     cName: "nav-text",
   },
-]
+];
 
 export default SidebarData;
